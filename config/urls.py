f72d7ef7cd0d99410b57e62b7cadd4f2d37c877d
KEREM CAN ÇELEPKOLU <kerem00962@gmail.com>
2024-02-01 02:59:40 +0300

    add: functionality openai

--- config/urls.py
+++ config/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from config.views import main_home
+from config.views import main_home,chatbot
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', main_home, name='main_home'),
+    path('chatbot/', chatbot, name='chatbot'),
 ]
--- config/views.py
+++ config/views.py
@@ -1,5 +1,36 @@
 from django.shortcuts import render
+import openai, os
+from dotenv import load_dotenv
+load_dotenv()
 
+api_key = os.getenv('OPENAI_KEY', None)
+openai.api_key = api_key
+
+def chatbot(request):
+    #  Configure OpenAI API client with your api key
+    chatbot_response = None
+    # If the user has submitted a question
+    print("before if statement")
+    if api_key is not None and request.method == 'POST':
+        print("inside if statement")
+        user_input = request.POST.get('user_input')
+        # prompt = user_input 
+        prompt = f"translate this text to Kurdish and use Latin alphabet: {user_input}"
+        response = openai.completions.create(
+            model="gpt-3.5-turbo-instruct",
+            prompt=prompt,
+            temperature=0.5,
+            max_tokens=256,
+            # stop="." # Stop the sequence on the first period.
+        )
+        print("before response")
+        print(response)
+        chatbot_response = response.choices[0].text
+    
+    context = dict(
+        response_chatbot=chatbot_response,
+    )
+    return render(request, 'core/main.html',context)
 
 def main_home(request):
     return render(request, 'core/base.html')
